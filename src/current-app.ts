6d02624b20f8796f6a7b0e21cc03261d55ac9c04
rutan <ru_shalm@hazimu.com>
2022-05-03 21:14:34 +0900

    upgrade and remove modules

--- src/current-app.ts
+++ src/current-app.ts
@@ -1,5 +1,4 @@
 import {exec} from "child_process";
-import * as fs from "fs";
 import * as path from "path";
 
 const scriptPath = path.join(__dirname, "..", "script", "mac.js");
--- src/slack-command.ts
+++ src/slack-command.ts
@@ -1,37 +1,29 @@
-import * as Slack from "slack-node";
+import { WebClient } from "@slack/web-api";
 import { Credential } from "./credential";
 
 export function authTest(credential: Credential): Promise<void> {
-    return new Promise((resolve, reject) => {
-        const slack = new Slack(credential.slackToken);
-        slack.api("auth.test", (err, response) => {
-            if (err) { return reject(err); }
-            response.ok ? resolve() : reject(response.error);
-        });
+    const slack = new WebClient(credential.slackToken);
+    return slack.auth.test().then((resp) => {
+        if (!resp.ok) { return Promise.reject(resp); }
     });
 }
 
 export function fetchEmojiNameList(credential: Credential): Promise<string[]> {
-    return new Promise((resolve, reject) => {
-        const slack = new Slack(credential.slackToken);
-        slack.api("emoji.list", (err, response) => {
-            if (err) { return reject(err); }
-            response.ok ? resolve(Object.keys(response.emoji).sort()) : reject(response.error);
-        });
+    const slack = new WebClient(credential.slackToken);
+    return slack.emoji.list().then((response) => {
+        if (!response.ok) { return Promise.reject(response); }
+        return Object.keys(response.emoji).sort();
     });
 }
 
 export function updateStatus(credential: Credential, emoji: string, text: string): Promise<void> {
-    return new Promise((resolve, reject) => {
-        const slack = new Slack(credential.slackToken);
-        slack.api("users.profile.set", {
-            profile: JSON.stringify({
-                status_emoji: emoji ? `:${emoji}:` : "",
-                status_text: text,
-            }),
-        }, (err, response) => {
-            if (err) { return reject(err); }
-            response.ok ? resolve() : reject(response.error);
-        });
+    const slack = new WebClient(credential.slackToken);
+    return slack.users.profile.set({
+        profile: JSON.stringify({
+            status_emoji: emoji ? `:${emoji}:` : "",
+            status_text: text,
+        }),
+    }).then((resp) => {
+        if (!resp.ok) { return Promise.reject(resp); }
     });
 }
